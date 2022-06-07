2e9c92b3463347639bd290614790f8e333d6574c
Germanna Rebouças <reboucasgermanna@gmail.com>
2022-06-06 21:47:23 -0300

    Create AplicacaoWeb_Flask.py

---
+++ AplicacaoWeb_Flask.py
@@ -0,0 +1,45 @@
+
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+'''
+@app.route("/")
+def home():
+    return "Olá! Aula de Algoritmos usando Flask <h1> Criando pags com Flask </h1>"
+
+if __name__ == "__main__":
+    app.run()
+'''
+
+@app.route("/")
+def home():
+
+    return "Olá! Aula de Algoritmos usando Flask <h1> Criando pags com Flask </h1>"
+
+
+@app.route("/cadastrodealunos")
+def cadastrodealunos():
+
+    return "Cadastro de alunos! <h1> Tela reservada para construção da pagina de  cadastro de alunos </h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f'Hello {name}!'
+
+@app.route("/admin")
+def admin():
+    return redirect((url_for("home")))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+    '''3 links:
+    1. cadastro -> Form de Cadastro de alunos
+    2. informações
+    3. Seja bem vindo'''
